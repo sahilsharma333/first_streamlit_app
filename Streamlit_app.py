8da1786391d62e7ee6c0f197f6d8479158856a68
Sahil Sharma <66203750+sahilsharma333@users.noreply.github.com>
2023-02-23 15:25:07 +0530

    Update Streamlit_app.py
    Read csv file to get smoothie ingredients

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text(' Burger')
 streamlit.text('綾 Salad')
 streamlit.header('Build your own Fruit Smoothie')
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
